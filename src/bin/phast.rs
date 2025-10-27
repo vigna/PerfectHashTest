c735a525c54d087f608ca2a0de49e86a9275c6b9
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-10-27 11:28:52 +0100

    Phast+ test

---
+++ src/bin/phast.rs
@@ -0,0 +1,35 @@
+use core::hint::black_box;
+use std::time::Instant;
+
+use clap::Parser;
+use ph::phast;
+
+#[derive(Parser, Debug)]
+#[command(about = "Build contributor and origin collaboration graphs")]
+struct Args {
+    n: usize,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let n = args.n;
+    let keys: Vec<_> = (0..n).collect();
+    let now = Instant::now();
+    let phast = phast::Function2::from_slice_mt(&keys);
+    println!(
+        "Phast construction time {:8.3}ns/key",
+        now.elapsed().as_nanos() as f64 / n as f64
+    );
+
+    for _ in 0..2 {
+        let now = Instant::now();
+        for i in 0..n {
+            _ = black_box(phast.get(&i));
+        }
+        println!(
+            "Phast   lookup time {:8.3}ns/key",
+            now.elapsed().as_nanos() as f64 / n as f64
+        );
+    }
+}
